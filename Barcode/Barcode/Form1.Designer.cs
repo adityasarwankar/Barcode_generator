77f4e894c643fbfad0b40dd8a218895ce3e514fc
Aditya Sarwankar <42802074+adityasarwankar@users.noreply.github.com>
2019-12-22 11:50:09 +0530

    Add files via upload

---
+++ Barcode/Barcode/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Column5 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(807, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(171, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 34);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 133);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 202);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(171, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column1,
+            this.Column2,
+            this.Column4,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(483, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 239);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Batch No:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Shop_ID",
+            "Shop_Name",
+            "Route_ID",
+            "City_ID "});
+            this.comboBox1.Location = new System.Drawing.Point(436, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 33);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Select";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Shop_ID";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Shop_Name";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Rout_Id";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "City_ID";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 484);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Barcode/Barcode/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+
+
+namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-S9B711P\\SQLEXPRESS;Initial Catalog=npb;User ID=sa;Password=sa@1234");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string batch = textBox1.Text;
+
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+                if((bool)item.Cells[0].Value == true)
+                {
+                    string prod = item.Cells[2].Value.ToString();
+                    string mrp = item.Cells[1].Value.ToString();
+                    string str = item.Cells[2].Value.ToString();
+
+                    Bitmap bitmap = new Bitmap(str.Length * 50, 150);
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
+                    {
+                        Font ofont = new System.Drawing.Font("3 of 9 Barcode", 40);
+                        Font ifont = new System.Drawing.Font("Arial", 10);
+                        Font iifont = new System.Drawing.Font("Arial", 10);
+                        PointF oiopoint = new PointF(50f, 2f);
+                        PointF oipoint = new PointF(50f, 23f);
+                        PointF point = new PointF(25f, 48f);
+                        PointF ipoint = new PointF(100f, 100f);
+
+
+                        SolidBrush black = new SolidBrush(Color.Black);
+                        SolidBrush white = new SolidBrush(Color.White);
+                        graphics.FillRectangle(white, 0, 0, bitmap.Width, bitmap.Height);
+                        graphics.DrawString(prod, iifont, black, oiopoint);
+                        graphics.DrawString(str, ofont, black, point);
+                        graphics.DrawString(batch, ifont, black, ipoint);
+                        graphics.DrawString("mrp-" + mrp, ifont, black, oipoint);
+
+                    }
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bitmap.Save(ms, ImageFormat.Png);
+                        pictureBox1.Image = bitmap;
+                        pictureBox1.Height = bitmap.Height;
+                        pictureBox1.Width = bitmap.Width;
+                    }
+                }
+            }
+                      
+        }
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if ((bool)dataGridView1.SelectedRows[0].Cells[0].Value == false)
+            {
+                dataGridView1.SelectedRows[0].Cells[0].Value = true;
+            }
+            else
+            {
+                dataGridView1.SelectedRows[0].Cells[0].Value = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "JPEG(*.JPEG)|*.jpg" })
+            {
+                if (pictureBox1 == null)
+                    return;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.Text == "Shop_ID")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("select Shop_ID,Shop_Name, Route_ID,City_ID from tbl_ShopMaster WHERE Shop_ID LIKE '" + textBox1.Text + "%'", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.Rows.Clear();
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[1].Value = item["Shop_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[2].Value = item["Shop_Name"].ToString();
+                    dataGridView1.Rows[n].Cells[3].Value = item["Route_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[4].Value = item["City_ID"].ToString();
+                }
+            }
+
+            if (comboBox1.Text == "Shop_Name")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("select Shop_ID,Shop_Name, Route_ID,City_ID from tbl_ShopMaster WHERE Shop_Name LIKE '" + textBox1.Text + "%'", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.Rows.Clear();
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[1].Value = item["Shop_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[2].Value = item["Shop_Name"].ToString();
+                    dataGridView1.Rows[n].Cells[3].Value = item["Route_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[4].Value = item["City_ID"].ToString();
+                }
+            }
+
+            if (comboBox1.Text == "Route_ID")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("select Shop_ID,Shop_Name, Route_ID,City_ID from tbl_ShopMaster WHERE Route_ID LIKE '" + textBox1.Text + "%'", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.Rows.Clear();
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[1].Value = item["Shop_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[2].Value = item["Shop_Name"].ToString();
+                    dataGridView1.Rows[n].Cells[3].Value = item["Route_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[4].Value = item["City_ID"].ToString();
+                }
+            }
+
+            if (comboBox1.Text == "City_ID")
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("select Shop_ID,Shop_Name, Route_ID,City_ID from tbl_ShopMaster WHERE City_ID LIKE '" + textBox1.Text + "%'", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.Rows.Clear();
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[1].Value = item["Shop_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[2].Value = item["Shop_Name"].ToString();
+                    dataGridView1.Rows[n].Cells[3].Value = item["Route_ID"].ToString();
+                    dataGridView1.Rows[n].Cells[4].Value = item["City_ID"].ToString();
+                }
+            }
+
+
+        }
+    }
+}
